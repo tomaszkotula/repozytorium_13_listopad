7be97f7cba06d26fccf68e734e8bb113882af4e5
Tomasz Kotula <biotomix1@gmail.com>
2023-12-11 20:19:40 +0200

    Suma zmiennych

---
+++ zadanie_29_js_suma_zmiennych/main.js
@@ -0,0 +1,9 @@
+let firstNumber=10;
+let secondNumber=20;
+function Suma (firstNumber,secondNumber){
+    let Wynik=firstNumber+secondNumber;
+    return Wynik;
+}
+let Wynik=Suma(firstNumber,secondNumber);
+ 
+console.log(Wynik);
