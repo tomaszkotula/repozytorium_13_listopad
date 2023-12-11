afea0fd2991345063fa0fd73ceb08a6e09f111b6
Tomasz Kotula <biotomix1@gmail.com>
2023-12-11 18:04:27 +0200

    Warunki

---
+++ zadanie_28_js_warunki/main.js
@@ -0,0 +1,8 @@
+let Zmienna1=12;
+if (Zmienna1 > 10) {
+    console.log("Liczba jest większa od 10");
+    }
+    else if (Zmienna1<10){
+   console.log("Liczba nie jest większa od 10");
+    }
+    
