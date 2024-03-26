5cb0e333e2b96fbd8f3d5f03d240ac17983e85cb
tomaszkotula <149460249+tomaszkotula@users.noreply.github.com>
2024-03-26 10:29:01 +0200

    Delete JAVA SCRIPT/PRZYKŁAD 3 directory

--- JAVA SCRIPT/PRZYKŁAD 3/js/script.js
+++
@@ -1,8 +0,0 @@
-function addNumbers(parametr1,  parametr2) {
-    let result = parametr1 + parametr2;    
-    return result;
-}
-
-let sum1 = addNumbers(1,2);
-let sum2 = addNumbers(5,6);
-console.log(sum2);
